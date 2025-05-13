cad3022e065a412827fba01e5c74c47c3d68cd77
Shimouna-14 <chimene.ndeby@gmail.com>
2025-05-13 16:30:30 +0200

    Ajout de la restriction d'accès pour le contrôleur UserController aux utilisateurs avec le rôle ADMIN

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/dashboard/admin', name: 'app_admin', methods: ['GET', 'POST'])]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
