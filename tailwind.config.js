f012ef75ab390bc9eb12fb3229b0d1174eb0c932
Shimouna-14 <chimene.ndeby@gmail.com>
2025-05-15 15:58:12 +0200

    Réorganisation des couleurs et ajout de dégradés d'arrière-plan dans la configuration de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,17 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "primary-red":"#971313",
-                "secondary-red":"#AA4141",
-                "primary-black":"#2C2C2C",
-                "secondary-black":"#4F4F59",
-                "primary-white":"#F7F7F7",
-                "secondary-white":"#E4E2E3",
-                "tertiary-white":"#D7D5D6",
-
+                "primary-red": "#971313",
+                "secondary-red": "#AA4141",
+                "primary-black": "#2C2C2C",
+                "secondary-black": "#4F4F59",
+                "primary-white": "#F7F7F7",
+                "secondary-white": "#E4E2E3",
+                "tertiary-white": "#D7D5D6",
             },
-            fontFamily: {
+            backgroundImage: {
+                'red-to-black': 'linear-gradient(to bottom, #AA4141 80%, #2C2C2C 100%)',
+                'black-to-red': 'linear-gradient(to bottom, #2C2C2C 10%, #AA4141 100%)',
             },
         },
     },
