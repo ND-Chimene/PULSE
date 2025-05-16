0e2b6054d2384523c4f878fb551b0f04c47ff6f4
Shimouna-14 <chimene.ndeby@gmail.com>
2025-05-16 17:12:19 +0200

    Refactor le contrôleur utilisateur : mise à jour de la méthode index pour gérer le formulaire utilisateur et la validation du mot de passe.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[IsGranted('ROLE_ADMIN')]
 final class UserController extends AbstractController
 {
-    #[Route('/dashboard/admin', name: 'app_admin', methods: ['GET', 'POST'])]
-    public function index(): Response
+
+    public function __construct(private EntityManagerInterface $em) {}
+
+    #[Route('/admin', name: 'app_admin', methods: ['GET', 'POST'])]
+    public function index(Request $request, UserPasswordHasherInterface $ph): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setPassword($ph->hashPassword($user, $form->get('password')->getData()));
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_admin');
+        }
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            "form" => $form,
         ]);
     }
 }
