9d9ade87aab90c2cfd31699f19d5c0b34d45a085
NDEBY Véronique Chimène <80843218+ND-Chimene@users.noreply.github.com>
2026-05-22 17:47:12 +0200

    Configuration Docker : mise à jour des fichiers Dockerfile et compose.yaml, ajout de la gestion des certificats SSL avec Certbot, et amélioration du script d'initialisation PHP.

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
-chown -R www-data:www-data var
-exec php-fpm
+set -e
+
+export APP_ENV=prod
+export APP_DEBUG=0
+
+echo "🚀 Starting app..."
+
+mkdir -p var/cache var/log var/sessions/prod
+
+echo "🧨 Reset database..."
+
+php bin/console doctrine:database:drop --force --if-exists --env=prod || true
+php bin/console doctrine:database:create --env=prod
+
+# IMPORTANT : reset complet du schéma Doctrine
+php bin/console doctrine:schema:drop --force --full-database --env=prod || true
+php bin/console doctrine:schema:create --env=prod
+
+echo "🧱 Running migrations..."
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+
+echo "🧹 Cache..."
+php bin/console cache:clear --env=prod --no-debug || true
+php bin/console cache:warmup --env=prod --no-debug || true
+
+chown -R www-data:www-data var || true
+
+exec php-fpm -F
